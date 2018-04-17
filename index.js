918d4a0079ad804b70fda0a3725981d5aa6fde06
LeShaunJ <leshaun.john@gmail.com>
2018-04-17 08:56:28 -0600

    More documenting; fixed QRY class

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 /////////////////////////////////////////////////////////////////////////////////
 // EXPORTS
 
+	/**
+	 * A collection of SQL Utilities & MySQL connector
+	 * @module dffrnt.model
+	 */
 	module.exports = {
 		SQL: 		require('./lib/sql'),
 		Connection: require('./lib/connection')
