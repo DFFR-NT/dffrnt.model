368c2fca3d41514692554e564a3307ccb1093386
LeShaunJ <leshaun.john@gmail.com>
2018-05-21 10:41:52 -0600

    Added more docs; fixed bugs; upped to v1.5.2

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 	/**
 	 * A collection of SQL Utilities & MySQL connector
 	 * @module dffrnt.model
+	 * @prop	{SQL} SQL
+	 * @prop	{Connection} Connection
 	 */
 	module.exports = {
 		SQL: 		require('./lib/sql'),
