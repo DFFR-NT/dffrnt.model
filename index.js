b53e08037e5797f7150f9a656acc9a85be9de3ea
LeShaunJ <leshaun.john@gmail.com>
2019-12-23 20:38:21 -0700

    Upped to dffrnt.api v4.0.0

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+/**
+ * A collection of SQL Utilities & MySQL connector
+ * @module dffrnt.model
+ */
 
 'use strict';
 
 /////////////////////////////////////////////////////////////////////////////////
 // EXPORTS
 
-	/**
-	 * A collection of SQL Utilities & MySQL connector
-	 * @module dffrnt.model
-	 * @prop	{SQL} SQL
-	 * @prop	{Connection} Connection
-	 */
 	module.exports = {
 		SQL: 		require('./lib/sql'),
 		Connection: require('./lib/connection')
